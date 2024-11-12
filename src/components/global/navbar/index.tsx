cfa0b266e6cd80cd55388bea28a9669841d21821
tanzid64 <tanzid3@gmail.com>
2024-11-13 00:15:58 +0600

    feat: Navbar created

---
+++ src/components/global/navbar/index.tsx
@@ -0,0 +1,80 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+import { SignOutButton } from "@clerk/nextjs";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+import { FC } from "react";
+import { MaxWidthWrapper } from "../max-width-wrapper";
+
+interface NavbarProps {}
+
+export const Navbar: FC<NavbarProps> = ({}) => {
+  // TODO: Add auth
+  const user = false;
+
+  return (
+    <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/80 backdrop-blur-lg transition-all">
+      <MaxWidthWrapper>
+        <div className="flex h-16 items-center justify-between">
+          <Link href={"/"} className="flex z-40 font-semibold">
+            Ping <span className="text-brand-700">Panda</span>
+          </Link>
+          <div className="h-full flex items-center space-x-4">
+            {user ? (
+              <>
+                <SignOutButton>
+                  <Button size={"sm"} variant={"ghost"}>
+                    Sign out
+                  </Button>
+                </SignOutButton>
+                <Link
+                  href="/dashboard"
+                  className={buttonVariants({
+                    size: "sm",
+                    className: "flex items-center gap-1",
+                  })}
+                >
+                  Dashboard
+                  <ArrowRight className="size-4 ml-1.5" />
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link
+                  href="/dashboard"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  Pricing
+                </Link>
+                <Link
+                  href="/sign-in"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  Sign in
+                </Link>
+
+                <div className="h-8 w-px bg-gray-200" />
+
+                <Link
+                  href="/sign-up"
+                  className={buttonVariants({
+                    size: "sm",
+                    className: "flex items-center gap-1",
+                  })}
+                >
+                  Sign up
+                  <ArrowRight className="size-4" />
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  );
+};
