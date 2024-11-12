c053994386a55a62a2aa8f3f56d7b747c7b54923
tanzid64 <tanzid3@gmail.com>
2024-11-12 20:32:49 +0600

    feat: hero section completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,65 @@
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query"
-import { RecentPost } from "./components/post"
-import { client } from "./lib/client"
+import { Check } from "lucide-react";
+import { FC } from "react";
+import { Heading } from "./components/global/heading";
+import { MaxWidthWrapper } from "./components/global/max-width-wrapper";
+import { ShinyButton } from "./components/global/shiny-button";
 
-export default async function Home() {
-  const res = await client.post.recent.$get()
-  const recentPost = await res.json()
-
-  /**
-   * This is the intended way to prefetch data on the server to have it immediately available in the client.
-   * But: you could also just pass the post as a prop instead of using the `HydrationBoundary`
-   */
-  const queryClient = new QueryClient()
-
-  await queryClient.prefetchQuery({
-    queryFn: () => recentPost,
-    queryKey: ["get-recent-post"],
-  })
-
+const Page: FC = () => {
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <main className="flex min-h-screen flex-col items-center justify-center">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-medium font-heading tracking-tight sm:text-[5rem]">
-            <span className="text-brand-700">j</span>stack
-          </h1>
+    <>
+      {/* Hero Section */}
+      <section className="relative py-24 sm:py-32 bg-brand-25">
+        <MaxWidthWrapper className="text-center">
+          <div className="relative mx-auto text-center flex flex-col gap-10 items-center">
+            {/* Header */}
+            <div>
+              <Heading>
+                <span>Real-Time SaaS Insights,</span>
+                <br />
+                <span className="relative bg-gradient-to-r from-brand-700 to-brand-800 text-transparent bg-clip-text">
+                  Delivered to Your Discord
+                </span>
+              </Heading>
+            </div>
+            {/* Description */}
+            <p className="text-base/7 text-gray-600 max-w-prose text-center text-pretty">
+              PingPanda is the easiest way to monitor your SaaS. Get instant
+              notifications for{" "}
+              <span className="font-semibold text-gray-700">
+                sales, new users, or any other event
+              </span>{" "}
+              sent directly to your Discord.
+            </p>
+            {/* Features */}
+            <ul className="space-y-2 text-base/7 text-gray-600 text-left flex flex-col items-start">
+              {[
+                "Real-time Discord alerts for critical events",
+                "Buy once, use forever",
+                "Track sales, new users, or any other event",
+              ].map((item, index) => (
+                <li key={index} className="flex gap-1.5 items-center text-left">
+                  <Check className="size-5 shrink-0 text-brand-700" />
+                  {item}
+                </li>
+              ))}
+            </ul>
+            {/* Button */}
+            <div className="w-full max-w-80">
+              <ShinyButton
+                href="/sign-up"
+                className="relative z-10 h-14 w-full text-base shadow-lg transition-shadow duration-300 hover:shadow-xl"
+              >
+                Start For Free Today
+              </ShinyButton>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+      <section></section>
+      <section></section>
+      <section></section>
+    </>
+  );
+};
 
-          <p className="text-center max-w-prose text-balance">
-            The modern way to confidently ship high-performance, low-cost
-            Next.js apps. End-to-end typesafe with an incredible DX.
-          </p>
-
-          <RecentPost />
-        </div>
-      </main>
-    </HydrationBoundary>
-  )
-}
+export default Page;
