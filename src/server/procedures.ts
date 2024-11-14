5405fe163a2bc3b5296cbcfaefef7fc46b5554db
tanzid64 <tanzid3@gmail.com>
2024-11-14 20:33:38 +0600

    feat: getEventCategories api created

--- src/server/procedures.ts
+++ src/server/procedures.ts
@@ -1,14 +1,43 @@
-import { j } from "./__internals/j"
+import { db } from "@/db";
+import { currentUser } from "@clerk/nextjs/server";
+import { HTTPException } from "hono/http-exception";
+import { j } from "./__internals/j";
 
-const authMiddleware = j.middleware(async ({ ctx, next }) => {
-  return next({})
-})
+const authMiddleware = j.middleware(async ({ c, next }) => {
+  const authHeader = c.req.header("Authorization");
 
-/**
+  if (authHeader) {
+    const apiKey = authHeader.split(" ")[1]; // bearer <API_KEY>
+    // get user by apiKey from user model
+    const user = await db.user.findUnique({
+      where: { apiKey },
+    });
+
+    if (user) return next({ user,db });
+  }
+
+  const auth = await currentUser();
+
+  if (!auth) {
+    throw new HTTPException(401, { message: "Unauthorized" });
+  }
+
+  const user = await db.user.findUnique({
+    where: { clerkId: auth.id },
+  });
+
+  if (!user) {
+    throw new HTTPException(401, { message: "Unauthorized" });
+  }
+
+  return next({ user,db });
+});
+
+/*
  * Public (unauthenticated) procedures
  *
  * This is the base piece you use to build new queries and mutations on your API.
  */
-export const baseProcedure = j.procedure
-export const publicProcedure = baseProcedure
-export const privateProcedure = publicProcedure.use(authMiddleware)
+export const baseProcedure = j.procedure;
+export const publicProcedure = baseProcedure;
+export const privateProcedure = publicProcedure.use(authMiddleware);
---
+++ src/server/routers/category-router.ts
@@ -0,0 +1,74 @@
+import { startOfMonth } from "date-fns";
+import { router } from "../__internals/router";
+import { privateProcedure } from "../procedures";
+
+export const categoryRouter = router({
+  getEventCategories: privateProcedure.query(async ({ c, ctx }) => {
+    const now = new Date();
+    const firstDayOfMonth = startOfMonth(now);
+
+    const categories = await ctx.db.eventCategory.findMany({
+      where: { userId: ctx.user.id },
+      select: {
+        id: true,
+        name: true,
+        emoji: true,
+        color: true,
+        updatedAt: true,
+        createdAt: true,
+        // Get each categoriy's events for current month
+        events: {
+          where: {
+            createdAt: { gte: firstDayOfMonth },
+          },
+          select: {
+            fields: true,
+            createdAt: true,
+          },
+        },
+        // Get count of each category events for current month
+        _count: {
+          select: {
+            events: {
+              where: {
+                createdAt: { gte: firstDayOfMonth },
+              },
+            },
+          },
+        },
+      },
+      orderBy: { updatedAt: "desc" },
+    });
+
+    // Post-processing -> to get unique field count of each category, and the most recent event
+    const categoriesWithCounts = categories.map((category) => {
+      const uniqueFieldNames = new Set<string>();
+      let lastPing: Date | null = null;
+
+      category.events.forEach((event) => {
+        Object.keys(event.fields as object).forEach((fieldName) => {
+          uniqueFieldNames.add(fieldName);
+        });
+
+        if (!lastPing || event.createdAt > lastPing) {
+          lastPing = event.createdAt;
+        }
+      });
+
+      return {
+        id: category.id,
+        name: category.name,
+        emoji: category.emoji,
+        color: category.color,
+        updatedAt: category.updatedAt,
+        createdAt: category.createdAt,
+        uniqueFieldCount: uniqueFieldNames.size,
+        eventsCount: category._count.events,
+        lastPing,
+      };
+    });
+
+    // super json can handle dates but json can't -> more in superjson documentation
+    return c.superjson({ categories: categoriesWithCounts });
+  }),
+});
