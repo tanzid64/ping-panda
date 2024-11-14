9d7c2685734a4c05e5885aaa36de8090ab2438f7
tanzid64 <tanzid3@gmail.com>
2024-11-14 23:48:36 +0600

    feat: create event category api

--- src/server/routers/category-router.ts
+++ src/server/routers/category-router.ts
@@ -2,6 +2,8 @@ import { startOfMonth } from "date-fns";
 import { z } from "zod";
 import { router } from "../__internals/router";
 import { privateProcedure } from "../procedures";
+import { CATEGORY_NAME_VALIDATOR } from "@/lib/validators/category-validator";
+import { parseColor } from "@/lib/utils";
 
 export const categoryRouter = router({
   getEventCategories: privateProcedure.query(async ({ c, ctx }) => {
@@ -82,5 +84,34 @@ export const categoryRouter = router({
         where: { name_userId: { name, userId: ctx.user.id } },
       });
       return c.json({ success: true });
+    }),
+
+  createEventCategory: privateProcedure
+    .input(
+      z.object({
+        name: CATEGORY_NAME_VALIDATOR,
+        color: z
+          .string()
+          .min(1, "Color is required")
+          .regex(/^#[0-9A-F]{6}$/i, "Invalid color format."),
+        emoji: z.string().emoji("Invalid emoji").optional(),
+      })
+    )
+    .mutation(async ({ c, ctx, input }) => {
+      const { user } = ctx;
+      const { color, name, emoji } = input;
+
+      // TODO: ADD PAID PLAN LOGIC
+
+      const eventCategory = await ctx.db.eventCategory.create({
+        data: {
+          name: name.toLowerCase(),
+          color: parseColor(color),
+          emoji,
+          userId: user.id,
+        },
+      });
+
+      return c.json({ eventCategory });
     }),
 });
