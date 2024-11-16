293cf0972eea84c66a3511464fdde4b8b21a5548
tanzid64 <tanzid3@gmail.com>
2024-11-16 20:46:33 +0600

    feat: create stripe session api created

---
+++ src/server/routers/payment-router.ts
@@ -0,0 +1,14 @@
+import { createCheckoutSession } from "@/lib/stripe";
+import { router } from "../__internals/router";
+import { privateProcedure } from "../procedures";
+
+export const paymentRouter = router({
+  createCheckoutSession: privateProcedure.mutation(async ({ c, ctx }) => {
+    const { user, db } = ctx;
+    const session = await createCheckoutSession({
+      userEmail: user.email,
+      userId: user.id,
+    });
+    return c.json({ url: session.url });
+  }),
+});
